181a44a8d30831008eeeb8fb323234d3d7fd383a
Ales Šafařík <jas07245866@gmail.com>
2026-03-14 10:55:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5230144579-281f1",
+  "appId": "1:212594496461:web:e0505768b4a8b14df3aa84",
+  "apiKey": "AIzaSyCY3w3NG2GYE-dZjELiF-Z7JkJ0A5A4zSE",
+  "authDomain": "studio-5230144579-281f1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "212594496461"
+};
